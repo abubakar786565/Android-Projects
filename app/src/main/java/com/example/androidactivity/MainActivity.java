73d49cd8c2529553ec206c68e04c7f2ca32fc754
abubakar786565 <abubakarsuleheri@gmail.com>
2024-03-27 20:02:39 +0500

    first

--- app/src/main/java/com/example/androidactivity/MainActivity.java
+++ app/src/main/java/com/example/androidactivity/MainActivity.java
@@ -1,20 +1,46 @@
 package com.example.androidactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText name,phone,email;
+    private Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        name = findViewById(R.id.Name);
+        phone = findViewById(R.id.Phone);
+        email = findViewById(R.id.Email);
+        submit = findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String n = name.getText().toString();
+                String p = phone.getText().toString();
+                String e = email.getText().toString();
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("UserName",n);
+                intent.putExtra("phone",p);
+                intent.putExtra("email",e);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/androidactivity/MainActivity2.java
+++ app/src/main/java/com/example/androidactivity/MainActivity2.java
@@ -1,6 +1,7 @@
 package com.example.androidactivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +10,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
+    TextView Nameveiw,Phoneveiw,Emailveiw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+
+        Nameveiw  = findViewById(R.id.NameView) ;
+        Phoneveiw = findViewById(R.id.PhoneView) ;
+        Emailveiw = findViewById(R.id.EmailView) ;
+
+        String username = getIntent().getStringExtra("UserName");
+        String phoneNo    = getIntent().getStringExtra("phone");
+        String Email    = getIntent().getStringExtra("email");
+
+        Nameveiw.setText(username);
+        Phoneveiw.setText(phoneNo);
+        Emailveiw.setText(Email);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
